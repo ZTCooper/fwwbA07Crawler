0d109f5a12dc7c8a7d5140b9da51c3d7eead4401
ztcooper <ztcooper@outlook.com>
2018-01-13 21:44:13 +0800

    定义Item

--- jobSpider/jobSpider/items.py
+++ jobSpider/jobSpider/items.py
@@ -8,7 +8,22 @@
 import scrapy
 
 
-class JobspiderItem(scrapy.Item):
+class PositionItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    position_name = scrapy.Field()
+    position_salary = scrapy.Field()
+    position_descibe = scrapy.Field()
+
+
+class CompanyItem(scrapy.Item):
+    company_name = scrapy.Field()
+    company_location = scrapy.Field()
+    company_type = scrapy.Field()
+    company_size = scrapy.Field()
+    company_describe = scrapy.Field()
+
+
+class RequireItem(scrapy.Item):
+    aquire_experience = scrapy.Field()
+    aquire_education = scrapy.Field()
